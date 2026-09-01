8a3b8347a7bf4544e38e6c3faff7d32a3ed8d1e8
Gabriel Colucci <gacolucci1@gmail.com>
2026-09-01 15:43:29 -0300

    Delete Exercicio033_JogoVidaPersonagem.js

--- Exercicio033_JogoVidaPersonagem.js
+++
@@ -1,9 +0,0 @@
-let vida = Number(prompt("Digita a quantidade de vidas: "));
-
-if (vida <= 0){
-    console.log("Situação: Game Over!");
-}else if(vida <= 2){
-    console.log("Situação: Cuidado!");
-}else{
-    console.log("Situação: Tudo certo");
-}
