ca1b5fab52809dd52933d6b805219d64f0ca4413
Gabriel Colucci <gacolucci1@gmail.com>
2026-09-01 15:43:41 -0300

    Delete Exercicio034_Academia.js

--- Exercicio034_Academia.js
+++
@@ -1,9 +0,0 @@
-let academia_SugerirPlano = prompt("Qual é a sua frequencia semanal?: (a) 1-2 dias, (b) 3-4 dias, (c) +5 dias: ")
-
-if (academia_SugerirPlano == a){
-    console.log("Você gostaria de adquirir o plano Basico?");
-}else if(academia_SugerirPlano == b){
-    console.log("Você gostaria de adquirir o plano Intermediario?");
-}else{
-    console.log("Você gostaria de adquirir o plano Plano Premium?");
-}
