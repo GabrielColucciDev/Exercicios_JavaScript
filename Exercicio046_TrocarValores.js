0262c2eca95aae670b88c98525e3dd2718bad634
Gabriel Colucci <gacolucci1@gmail.com>
2026-09-01 15:46:10 -0300

    Delete Exercicio046_TrocarValores.js

--- Exercicio046_TrocarValores.js
+++
@@ -1,9 +0,0 @@
-let a = 5;
-let b = 10;
-let aux = a; 
-
-a = b;   // 'a' recebe o valor de 'b' (fica 10)
-b = aux; // 'b' recebe o valor antigo de 'a' (fica 5)
-
-console.log("a:", a);
-console.log("b:", b); 
